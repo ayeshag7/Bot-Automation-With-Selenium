042d46e95ad14e18c44af82f39157b3c6f6d412d
Ayesha <106478752+ayeshag7@users.noreply.github.com>
2022-09-12 10:07:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from Booking_Functionality.bookings import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    # bot.check_the_selected_currency()
-    # bot.check_the_selected_search_query()
-    # bot.change_the_currency(currency="USD")
-    # bot.change_the_search_query(search="bookinggo")
+    bot.check_the_selected_currency()
+    bot.check_the_selected_search_query()
+    bot.change_the_currency(currency="USD")
+    bot.change_the_search_query(search="bookinggo")
     bot.choose_the_destination(destination="Islamabad")
